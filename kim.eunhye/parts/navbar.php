3d5441464c6453629637b4b932580752d98d6941
kiimeh <90300343+kiimeh@users.noreply.github.com>
2021-10-25 15:03:04 +0900

    Add files via upload

--- kim.eunhye/parts/navbar.php
+++ kim.eunhye/parts/navbar.php
@@ -2,7 +2,7 @@
 <header class="navbar">
     <div class="container display-flex">
         <div class="flex-none">
-            <h1><a href="product_list.php">Furniture Showroom</a></h1>
+            <h1><a href="index.php">Furniture Showroom</a></h1>
         </div>
         <div class="flex-stretch"></div>
         <nav class="nav nav-flex flex-stretch">
@@ -13,7 +13,7 @@
                         <input type="search" class="hotdog" placeholder="Search">
                     </form>
                 </div>
-                <li><a href="product_list.php">Home</a></li>
+                <li><a href="index.php">Home</a></li>
                 <li><a href="product_furniture.php">Furniture</a></li>
                 <li><a href="product_about.php">About</a></li>
                 <li><a href="product_contact.php">Contact</a></li>
