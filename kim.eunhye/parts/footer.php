54fdd81c17c775381334e94e43443ffc2f984d74
kiimeh <90300343+kiimeh@users.noreply.github.com>
2021-10-25 13:35:36 +0900

    Add files via upload

---
+++ kim.eunhye/parts/footer.php
@@ -0,0 +1,27 @@
+<section class="footer">
+    <div class="social">
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-snapchat"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+    </div>
+
+    <ul class="list">
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">Services</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Terms</a>
+        </li>
+    </ul>
+    <p class="copyright">
+        Furniture Showroom @ 2021
+    </p>
+</section>
+
