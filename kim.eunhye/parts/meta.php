d03545927424bc58287a75ed5549f620f2818c70
kiimeh <90300343+kiimeh@users.noreply.github.com>
2021-11-01 14:03:07 +0900

    Delete kim.eunhye directory

--- kim.eunhye/parts/meta.php
+++
@@ -1,8 +0,0 @@
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-<link rel="stylesheet" href="lib/css/styleguide.css" />
-<link rel="stylesheet" href="lib/css/gridsystem.css" />
-<link rel="stylesheet" href="css/storetheme.css" />
-<script src="https://kit.fontawesome.com/855a6c101f.js" crossorigin="anonymous"></script>
